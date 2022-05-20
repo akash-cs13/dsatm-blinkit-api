610434ac2bb4c278ece850dfded0e1fa5d6e850d
Akash CS <84382172+akash-cs13@users.noreply.github.com>
2022-05-20 22:06:03 +0530

    initial commit

---
+++ blinkit.py
@@ -0,0 +1,72 @@
+import os
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+
+class Blinkit_api(webdriver.Chrome):
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.headless = True
+        chrome_options.add_argument("window-size=1920,1080")
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        super(Blinkit_api, self).__init__(service=Service(os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
+        super(Blinkit_api, self).__init__(options=chrome_options)
+        self.implicitly_wait(10)
+
+    def initialization(self):
+        self.get('https://www.blinkit.com')
+
+    def exit(self):
+        self.quit()
+
+    def set_location(self, pincode = '560028'):
+        self.find_element(by=By.CSS_SELECTOR, value='input[data-test-id="area-input-box"]').send_keys(pincode)
+        sleep(1)
+        self.find_element(by=By.CSS_SELECTOR, value='input[data-test-id="area-input-box"]').send_keys(Keys.ARROW_DOWN, Keys.ENTER)
+        sleep(2)
+
+    def search_for_product(self, product):
+        self.find_element(by=By.CSS_SELECTOR, value='input[placeholder="Search for products"]').send_keys(product, Keys.ENTER)
+
+        items = self.find_elements(by=By.CLASS_NAME, value="plp-product__name--box")
+        packet_desc = self.find_elements(by=By.CLASS_NAME, value="plp-product__quantity--box")
+        price = self.find_elements(by=By.CLASS_NAME, value="plp-product__price--new")
+        url = self.find_elements(by=By.CLASS_NAME, value="product__wrapper")
+
+        return_list = []
+        for i in range(0, len(items)):
+            return_list.append((items[i].text, packet_desc[i].text, price[i].text[1:], url[i].get_attribute("href")))
+
+
+        return return_list
+
+if __name__ == '__main__':
+    inst = Blinkit_api()
+    print("Initializing api.................................")
+    inst.initialization()
+    print("Setting up location.....................")
+    inst.set_location()
+    print("Initialization is done!")
+
+    while True:
+        read_string = input("\nEnter 'quit' if you want to exit \nSearch: ")
+
+        if read_string == "quit": break
+
+        product_list = inst.search_for_product(read_string)
+        for tup in product_list:
+            print(tup)
+        print("\n\n\n")
+
+    inst.quit()
+
+
+
+
---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify
+from blinkit_v2 import Blinkit_api
+
+app = Flask(__name__)
+
+blinkit = Blinkit_api()
+
+
+@app.route('/')
+def myfun():
+    return 'DSATM   Final year project'
+
+
+@app.route('/blinkit/init/<string:pincode>')
+def api1_init(pincode):
+    blinkit.initialization()
+    blinkit.set_location(pincode=pincode)
+    return jsonify({"blinkit-result": "Initialized"})
+
+@app.route('/blinkit/search/<string:product>', methods=['GET'])
+def api1(product):
+    mylist = blinkit.search_for_product(product=product)
+    temp_list = []
+    for item, packet_desc, price, url in mylist:
+        temp_list.append({"item": item, "packet_description": packet_desc, "price": price, "url": url})
+    return jsonify({"blinkit-result": "Success", "product": temp_list})
+
+@app.route('/blinkit/quit')
+def api1_quit():
+    blinkit.exit()
+    return jsonify({"blinkit-result": "Quit"})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
