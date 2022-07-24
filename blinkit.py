42274f5c480290f3b9408b46be78294805e9daa9
Akash CS <84382172+akash-cs13@users.noreply.github.com>
2022-07-24 23:01:05 +0530

    final commit

--- blinkit.py
+++ blinkit.py
@@ -1,4 +1,3 @@
-import os
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -7,7 +6,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import json
+import os
 
 class Blinkit_api(webdriver.Chrome):
     def __init__(self):
@@ -28,23 +28,47 @@ class Blinkit_api(webdriver.Chrome):
     def exit(self):
         self.quit()
 
-    def set_location(self, pincode = '560028'):
+    def set_location(self, pincode = '560004'):
+        #area = self.find_element(by=By.CSS_SELECTOR, value='input[data-test-id="area-input-box"]')
+        area = WebDriverWait(self, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[data-test-id="area-input-box"]')))
+
+        area.send_keys("Thayagaraja")
+        sleep(2)
+        area.send_keys("Nagar")
         sleep(3)
-        self.find_element(by=By.CSS_SELECTOR, value='input[data-test-id="area-input-box"]').send_keys(pincode)
-        sleep(3)
-        self.find_element(by=By.CSS_SELECTOR, value='input[data-test-id="area-input-box"]').send_keys(Keys.ARROW_DOWN, Keys.ENTER)
+        area.send_keys(Keys.ARROW_DOWN, Keys.ENTER)
+
+
 
     def search_for_product(self, product):
+        self.find_element(by=By.CSS_SELECTOR, value='input[placeholder="Search for products"]').send_keys(
+            Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE,
+            Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE,
+            Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE,
+            Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE,
+            Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE, Keys.BACKSPACE)
+        sleep(1)
         self.find_element(by=By.CSS_SELECTOR, value='input[placeholder="Search for products"]').send_keys(product, Keys.ENTER)
+        sleep(2)
 
-        items = self.find_elements(by=By.CLASS_NAME, value="plp-product__name--box")
+        items = self.find_elements(by=By.CLASS_NAME, value="Product__ProductName-sc-11dk8zk-4")
         packet_desc = self.find_elements(by=By.CLASS_NAME, value="plp-product__quantity--box")
-        price = self.find_elements(by=By.CLASS_NAME, value="plp-product__price--new")
+        price = self.find_elements(by=By.CLASS_NAME, value="ProductPrice__Price-sc-14194u2-1")
+        #url = self.find_elements(by=By.CSS_SELECTOR, value='a[class ="product__wrapper"  data-test-id="plp-product"]')
         url = self.find_elements(by=By.CLASS_NAME, value="product__wrapper")
+        img = self.find_elements(by=By.CLASS_NAME, value="sc-iBkjds")
+
+
+        #img = self.find_elements(by=By.CSS_SELECTOR, value='img[class="img-loader__img--plp"]')
+        #WebDriverWait(self, 5).until(EC.visibility_of_all_elements_located(img))
 
         return_list = []
         for i in range(0, len(items)):
-            return_list.append((items[i].text, packet_desc[i].text, price[i].text[1:], url[i].get_attribute("href")))
+            return_list.append((items[i].text, packet_desc[i].text, price[i].text[1:], url[i].get_attribute("href"), img[i].get_attribute("src")))
+            #img[i].get_attribute("src")
+            #return_list.append((items[i].text, packet_desc[i].text, price[i].text[1:], url[i].get_attribute("href")))
+
+
 
 
         return return_list
@@ -55,19 +79,30 @@ if __name__ == '__main__':
     inst.initialization()
     print("Setting up location.....................")
     inst.set_location()
-    print("Initialization is done!")
+    print("Initialization is done!!!!!!!!!\n")
 
-    while True:
-        read_string = input("\nEnter 'quit' if you want to exit \nSearch: ")
+    labels = ['Apple', 'Banana', 'Coconut', 'Curd', 'Guava', 'Mango', 'Milk', 'Mosambi', 'Muskmelon', 'Onion', 'Orange', 'Papaya', 'Pomegranate', 'Potato', 'Tomato']
+    #labels = ['Apple', 'Onion', 'Potato']
 
-        if read_string == "quit": break
+    json_write = {}
 
+    for read_string in labels:
         product_list = inst.search_for_product(read_string)
-        for tup in product_list:
-            print(tup)
-        print("\n\n\n")
+        print("Fetching data for "+ read_string)
+
+        temp_list = []
+        for item, packet_desc, price, url, img in product_list:
+            temp_list.append({"item": item, "packet_description": packet_desc, "price": price, "url": url, "img": img})
+
+
+        json_write[read_string] = temp_list
+        print('\n')
+
 
     inst.quit()
+    f = open("Blinkitapi.json", "w")
+    f.write(json.dumps(json_write))
+    f.close()
 
 
 
